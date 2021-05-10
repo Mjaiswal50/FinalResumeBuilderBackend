d980b93037b67d64034ba2a3507b8bc32e836844
Mayank Jaiswal <60596814+Mjaiswal50@users.noreply.github.com>
2021-05-10 20:59:04 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import {Environment} from "./environment.interface";
 
 export const environment: Environment = {
     production: true,
-    db_url: 'mongodb+srv://mjaiswal50:dbpassword@cluster0.5zlyy.mongodb.net/mydatabase?retryWrites=true&w=majority'
+    db_url: 'xyzmongocollections'
 };
